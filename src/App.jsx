5fa2647f1e2398454a6b1106db2c6a0b085a2160
gradeup-ai <basisconsulting2024@gmail.com>
2025-02-12 23:35:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Interview from "./pages/Interview";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/interview/:id" element={<Interview />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/interview/:id" element={<Interview />} />
+    </Routes>
   );
 }
 
 export default App;
+
