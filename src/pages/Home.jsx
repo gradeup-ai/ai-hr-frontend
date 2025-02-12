ae65d1eed631361146b570a78ecb81d40623cbc3
gradeup-ai <basisconsulting2024@gmail.com>
2025-02-12 18:33:11 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PopupRegistration from "../components/PopupRegistration";
+
+function Home() {
+  return (
+    <div>
+      <h1>Вакансия QA-Тестировщика</h1>
+      <p>Пройди интервью и получи приглашение!</p>
+      <PopupRegistration />
+    </div>
+  );
+}
+
+export default Home;
